c4cc0682b3223650903a2859f403b63dff66fe31
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-12-16 01:43:05 +0300

    createCampaign function created

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -29,4 +29,23 @@ contract Charitycampaign {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public onlyOwner returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
 }
