1d20b20c0ae54a0d52620b4e77cabfc92e5590fc
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-12-16 01:50:13 +0300

    donate function added the contract

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -48,4 +48,19 @@ contract Charitycampaign {
         return numberOfCampaigns - 1;
     }
 
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
 }
