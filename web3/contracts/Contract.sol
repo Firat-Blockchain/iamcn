b86bf2061065adcfde248a9a47267f92e03ae037
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-12-16 01:53:37 +0300

    donators func get

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -63,4 +63,8 @@ contract Charitycampaign {
         }
     }
 
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
 }
