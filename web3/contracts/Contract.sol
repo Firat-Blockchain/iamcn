1b3a00d2fe8dada0642736a2819284f1bb4dcb4d
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-12-16 01:34:49 +0300

    campaign struct created

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -13,4 +13,20 @@ contract Charitycampaign {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
+
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
 }
