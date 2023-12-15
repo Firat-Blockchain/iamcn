ebbac7ec62b9bdfa4aa2adcd6ce63502278f73a9
akdoganalibugra1 <akdoganalibugra@gmail.com>
2023-12-16 01:29:59 +0300

    ownable function added for the contract

--- web3/contracts/Contract.sol
+++ web3/contracts/Contract.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract Charitycampaign {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
 }
